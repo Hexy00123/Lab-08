29522e587b292e16722dfb5326ed097d0b6a605b
Ruslan Izmailov <r.izmajlov2018@yandex.ru>
2025-06-01 15:30:00 +0300

    ci: fix send_to_remote.sh

--- scripts/send_to_remote.sh
+++ scripts/send_to_remote.sh
@@ -2,7 +2,8 @@
 
 IP_ADDR="$1"
 
-scp -r ./backend default@"$IP_ADDR":~/project/backend
-scp -r ./frontend default@"$IP_ADDR":~/project/frontend
-scp -r ./models default@"$IP_ADDR":~/project/models
+ssh -o StrictHostKeyChecking=no default@"$IP_ADDR" "mkdir -p ~/project"
+scp -r ./backend default@"$IP_ADDR":~/project/
+scp -r ./frontend default@"$IP_ADDR":~/project/
+scp -r ./models default@"$IP_ADDR":~/project/
 scp -r ./docker-compose.yaml default@"$IP_ADDR":~/project
